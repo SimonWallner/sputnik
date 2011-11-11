8726428bda04d950d554cf35e767bd56b8ee1e05
Simon Wallner <me@simonwallner.at>
2011-11-11 21:10:16 +0100

    can move shit, but velocity decay is wonky

--- sputnik/src/component/SamplerBehaviour.cpp
+++ sputnik/src/component/SamplerBehaviour.cpp
@@ -2,6 +2,7 @@
 
 using namespace sputnik::component;
 using namespace kocmoc::core::component;
+using namespace kocmoc::core;
 
 using glm::vec3;
 
@@ -17,9 +18,17 @@ void SamplerBehaviour::onUpdate(float deltaT, float t)
 	UNUSED t;
 	
 	if (selectable->isSelected())
+	{
 		ob->scale = vec3(1.5f);
-	else if (selectable->isHovering())
-		ob->scale = vec3(1.2f);
-	else 
-		ob->scale = vec3(1);
+		ob->lambda = math::decayConst(0.5f);
+	}
+	else
+	{
+		ob->lambda = math::decayConst(0.05f);
+		
+		if (selectable->isHovering())
+			ob->scale = vec3(1.2f);
+		else 
+			ob->scale = vec3(1);
+	}
 }
